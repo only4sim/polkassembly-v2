6dee3c2e59e30a802c96baa11a564b0e5af4b752
Curious Cat <35210356+only4sim@users.noreply.github.com>
2026-06-05 18:41:29 +0000

    feat: update ESLint configuration for test files and adjust TypeScript paths

--- functions/test/createUserProfile.test.js
+++ functions/test/createUserProfile.test.js
@@ -1,6 +1,9 @@
 // Copyright 2019-2025 @polkassembly/polkassembly authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
+// Copyright 2019-2025 @polkassembly/polkassembly authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
 const test = require('node:test');
 const assert = require('node:assert/strict');
 
